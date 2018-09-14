f714b2c7fbf7155141c2df87710c95626ed55b8b
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-09-14 17:34:14 -0400

    Prepared some files for react. Don't be shocked with react-rails and webpacker on the gemfile. Also generated models for Itinerary and Events. Itinerary's name is set default to Itinerary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,17 @@
 class PagesController < ApplicationController
+  
+  
+  # TODO: changed location from yelp API call to take in longitude and latitude 
+
   def home
+    
     searched = params[:search]
+    if searched != nil
+      query = searched.gsub( /\W/, '-' )
+    end
     @response = RestClient::Request.execute(
       method: :get,
-      url: "https://api.yelp.com/v3/businesses/search?term=#{searched}&location=brooklyn&open_now=true",
+      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=brooklyn&open_now=true",
       headers: {"Authorization" => "Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx"}
       )
       @data = JSON.parse(@response)
@@ -17,7 +25,10 @@ class PagesController < ApplicationController
       @address_two = @locationTwo['location']['display_address'][0]
 
 
-      @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{searched}&sort_by=best&location.address=new+york+city&token=FGTPMLNV7K6MQVZZCC6S")
+      @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=new+york+city&token=FGTPMLNV7K6MQVZZCC6S")
       @req = JSON.parse(@datae.body_str)
+
   end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_203259) do
+ActiveRecord::Schema.define(version: 2018_09_14_201623) do
 
-  create_table "places", force: :cascade do |t|
-    t.string "title"
-    t.text "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "visited_by"
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "image_url"
+    t.time "start_time"
+    t.time "end_time"
+    t.integer "itinerary_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["itinerary_id"], name: "index_events_on_itinerary_id"
+  end
+
+  create_table "itineraries", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_itineraries_on_user_id"
   end
 
 end
