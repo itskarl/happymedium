ee1a4a3b7e66d29ec2d444cdf41cf152c302ebca
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-14 11:30:46 -0400

    why am i saving this

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,12 @@
 class PlacesController < ApplicationController
-  def index
-    @places = Place.order('created_at DESC')
 
+
+  def index
     @data = Curl::Easy.perform("https://api.foursquare.com/v2/venues/explore?client_id=MX0ZI1L0F3JCH1PKERLTESHN4IGNQVW0HUDWSGVCJT343TFH&client_secret=SOXTW42PVMRQCVXGHZJLDLZE24HFJIGHYU3IMA3GBYASESDM&v=20180323&limit=10&ll=40.7243,-74.0018&query=dinner")
     @req = JSON.parse(@data.body_str)
+    
+    @places = Place.order('created_at DESC')
 
-    @data2 = Curl::Easy.perform("https://api.foursquare.com/v2/venues/explore?client_id=MX0ZI1L0F3JCH1PKERLTESHN4IGNQVW0HUDWSGVCJT343TFH&client_secret=SOXTW42PVMRQCVXGHZJLDLZE24HFJIGHYU3IMA3GBYASESDM&v=20180323&limit=10&ll=40.7243,-74.0018&query=drinks")
-    @req2 = JSON.parse(@data.body_str)
   end
 
   def new
