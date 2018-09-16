0d2c101814f6ef5f595bd024c0fe57a9c5bb6e74
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-09-15 22:36:10 -0400

    Fixed that NAME SAVE PROBLEM. Somewhat

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,38 +1,19 @@
 class PagesController < ApplicationController
   
   
-  # TODO: changed location from yelp API call to take in longitude and latitude 
 
   def home
     
-    searched = params[:search]
-    if searched != nil
-      query = searched.gsub( /\W/, '-' )
-    end
-    @response = RestClient::Request.execute(
-      method: :get,
-      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=brooklyn&open_now=true",
-      headers: {"Authorization" => "Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx"}
-      )
-      @data = JSON.parse(@response)
-      @locationOne = @data.first[1][rand(0...@data.first[1].count)]
-      @locationTwo = @data.first[1][rand(0...@data.first[1].count)]
-      @loc_one = @locationOne['name']
-      @loc_two = @locationTwo['name']
-      @loc_one_img = @locationOne['image_url']
-      @loc_two_img = @locationTwo['image_url']
-      @address_one = @locationOne['location']['display_address'][0]
-      @address_two = @locationTwo['location']['display_address'][0]
+    fetch_api_data
 
+    new_itinerary
 
-      @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=new+york+city&token=FGTPMLNV7K6MQVZZCC6S")
-      @req = JSON.parse(@datae.body_str)
-
-      @itinerary = Itinerary.new
-
-      @itinerary = Itinerary.where(params[:id])
+    @itinerary = Itinerary.where(params[:id])
 
   end
 
+  def new_itinerary
+    @itinerary = Itinerary.new
+  end
 
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,13 +1,17 @@
 class Itinerary < ApplicationRecord
-  after_initialize :set_default_name
+  after_initialize :set_default_name, unless: :persisted? 
+  # before_save :set_default_name
   # belongs_to :user
   has_many :events
 
 
   private 
 
-  def set_default_name
-    self.name = "Itinerary"
+  # I guess in a way, this self.name is locked on the input field already as Itinerary so it wouldn't be left blank. If erased and left blank, it returns an empty string 
+  # At least it is saving the name! THANK THE HEAVENS! 
+
+  def set_default_name    
+      self.name ||= "Itinerary" 
   end
 
 end
