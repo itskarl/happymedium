fa6a103e8c29720898625994f0f67b21b5b59ca9
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-17 19:24:17 -0400

    fixed search form to include eventbrite for multiple price preferences

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     searched = params[:search]
     query = searched.gsub(/\W/, '-') unless searched.nil?
     cost = []
+    event_cost = ''
     if params[:filter].nil?
       filter = ''
     else
@@ -15,23 +16,25 @@ class ApplicationController < ActionController::Base
      if params[:cost].nil?
           cost = [1,2,3]
           event_cost = ''
+     elsif params[:cost].count == 1
+       cost =  params[:cost]
      else
         params[:cost].each do |param|
           cost<< param
         end
 
-        if cost.count > 3
-          event_cost = 'paid + free'
+        if cost.count > 2
+          event_cost = 'paid+free'
         elsif cost.count == 1 && cost[0] == '1'
           event_cost = 'free'
         elsif cost.count == 2 && cost.all?{|val| val != 1}
           event_cost = 'paid'
         else
-          event_cost = 'paid + free'
+          event_cost = 'paid+free'
         end
 
      end
-    p cost
+    p cost.count
 
     destination = if params[:location].nil?
                     'new+york'
@@ -39,13 +42,26 @@ class ApplicationController < ActionController::Base
                     params[:location].gsub(/\W/, '-')
                 end
     # TODO: just revert location back to #{destination} later on
+
+    if cost.count > 1
     @response = RestClient::Request.execute(
       method: :get,
 
-      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=#{destination}&open_now=true&limit=50&price=#{cost[0]},#{cost[1]}&#{filter[0]},#{filter[1]},#{filter[2]}",
+      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=new+york&open_now=true&limit=50&price=#{cost[0]},#{cost[1]}&#{filter[0]},#{filter[1]},#{filter[2]}",
 
       headers: { 'Authorization' => 'Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx' }
     )
+    else
+      @response = RestClient::Request.execute(
+        method: :get,
+
+        url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=new+york&open_now=true&limit=50&price=#{cost[0]}&#{filter[0]},#{filter[1]},#{filter[2]}",
+
+        headers: { 'Authorization' => 'Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx' }
+      )
+    end
+
+
     @data = JSON.parse(@response)
     @locationOne = @data.first[1][rand(0...@data.first[1].count)]
     @locationTwo = @data.first[1][rand(0...@data.first[1].count)]
@@ -60,7 +76,7 @@ class ApplicationController < ActionController::Base
 
     p @data.first[1].count
 
-    @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=#{destination}&price=#{event_cost}token=FGTPMLNV7K6MQVZZCC6S")
+    @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=#{destination}&price=#{event_cost}&token=FGTPMLNV7K6MQVZZCC6S")
 
     @req = JSON.parse(@datae.body_str)
   end
