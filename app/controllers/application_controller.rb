b7f49e2192eb13872065af0c9fd022d85811119d
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-17 10:35:40 -0400

    fixed location search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,32 @@
 class ApplicationController < ActionController::Base
-
-
-  # TODO: changed location from yelp API call to take in longitude and latitude 
-
+  # TODO: changed location from yelp API call to take in longitude and latitude
 
   def fetch_api_data
-
     searched = params[:search]
-    if searched != nil
-      query = searched.gsub( /\W/, '-' )
-    end
+    query = searched.gsub(/\W/, '-') unless searched.nil?
+
+    destination = if params[:location].nil?
+                    'new+york'
+                  else
+                    params[:location].gsub(/\W/, '-')
+                end
+
     @response = RestClient::Request.execute(
       method: :get,
-      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=brooklyn&open_now=true",
-      headers: {"Authorization" => "Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx"}
-      )
-      @data = JSON.parse(@response)
-      @locationOne = @data.first[1][rand(0...@data.first[1].count)]
-      @locationTwo = @data.first[1][rand(0...@data.first[1].count)]
-      @loc_one = @locationOne['name']
-      @loc_two = @locationTwo['name']
-      @loc_one_img = @locationOne['image_url']
-      @loc_two_img = @locationTwo['image_url']
-      @address_one = @locationOne['location']['display_address'][0]
-      @address_two = @locationTwo['location']['display_address'][0]
-  
-  
-      @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=new+york+city&token=FGTPMLNV7K6MQVZZCC6S")
-      @req = JSON.parse(@datae.body_str)
-  
-  end 
+      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=#{destination}&open_now=true",
+      headers: { 'Authorization' => 'Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx' }
+    )
+    @data = JSON.parse(@response)
+    @locationOne = @data.first[1][rand(0...@data.first[1].count)]
+    @locationTwo = @data.first[1][rand(0...@data.first[1].count)]
+    @loc_one = @locationOne['name']
+    @loc_two = @locationTwo['name']
+    @loc_one_img = @locationOne['image_url']
+    @loc_two_img = @locationTwo['image_url']
+    @address_one = @locationOne['location']['display_address'][0]
+    @address_two = @locationTwo['location']['display_address'][0]
+
+    @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=#{destination}&token=FGTPMLNV7K6MQVZZCC6S")
+    @req = JSON.parse(@datae.body_str)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_09_15_234118) do
-
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -33,6 +31,16 @@ ActiveRecord::Schema.define(version: 2018_09_15_234118) do
     t.datetime "updated_at", null: false
     t.json "events_list"
     t.index ["user_id"], name: "index_itineraries_on_user_id"
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "title"
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "visited_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
