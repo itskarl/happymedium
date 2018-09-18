4262ded924fc2a9a86868cf7a7ceed999491b0e4
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-18 09:46:40 -0400

    working on sorting events based on current user location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,34 +6,41 @@ class ApplicationController < ActionController::Base
     searched = params[:search]
     query = searched.gsub(/\W/, '-') unless searched.nil?
     cost = []
+    loc_miles = params[:miles_away].to_i/0.00062137 if !params[:miles_away].nil?
+    event_miles = params[:miles_away]
+    p loc_miles
     event_cost = ''
-    if params[:filter].nil?
-      filter = ''
-    else
-      filter = params[:filter]
-    end
 
-     if params[:cost].nil?
+      if params[:filter].nil?
+        filter = ''
+      else
+        filter = params[:filter]
+      end
+
+       if params[:cost].nil?
           cost = [1,2,3]
           event_cost = ''
-     elsif params[:cost].count == 1
-       cost =  params[:cost]
-     else
-        params[:cost].each do |param|
-          cost<< param
+       elsif params[:cost].count == 1
+         cost = params[:cost]
+       else
+          params[:cost].each do |param|
+            cost<< param
+          end
+
+          if cost.count > 2
+            event_cost = 'paid+free'
+          elsif cost.count == 1 && cost[0] == '1'
+            event_cost = 'free'
+          elsif cost.count == 2 && cost.all?{|val| val != 1}
+            event_cost = 'paid'
+          else
+            event_cost = 'paid+free'
+          end
         end
 
-        if cost.count > 2
-          event_cost = 'paid+free'
-        elsif cost.count == 1 && cost[0] == '1'
-          event_cost = 'free'
-        elsif cost.count == 2 && cost.all?{|val| val != 1}
-          event_cost = 'paid'
-        else
-          event_cost = 'paid+free'
-        end
 
-     end
+
+
     p cost.count
 
     destination = if params[:location].nil?
@@ -44,18 +51,18 @@ class ApplicationController < ActionController::Base
     # TODO: just revert location back to #{destination} later on
 
     if cost.count > 1
-    @response = RestClient::Request.execute(
-      method: :get,
+      @response = RestClient::Request.execute(
+        method: :get,
 
-      url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=new+york&open_now=true&limit=50&price=#{cost[0]},#{cost[1]}&#{filter[0]},#{filter[1]},#{filter[2]}",
+        url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=new+york&open_now=true&limit=50&price=#{cost[0]},#{cost[1]}&#{filter[0]},#{filter[1]},#{filter[2]}&radius#{loc_miles}",
 
-      headers: { 'Authorization' => 'Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx' }
-    )
+        headers: { 'Authorization' => 'Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx' }
+      )
     else
       @response = RestClient::Request.execute(
         method: :get,
 
-        url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=new+york&open_now=true&limit=50&price=#{cost[0]}&#{filter[0]},#{filter[1]},#{filter[2]}",
+        url: "https://api.yelp.com/v3/businesses/search?term=#{query}&location=new+york&open_now=true&limit=50&price=#{cost[0]}&#{filter[0]},#{filter[1]},#{filter[2]}&radius#{loc_miles}",
 
         headers: { 'Authorization' => 'Bearer N8S3U6LDLLsusNB1-x8lUUwT6VzK8Vrz_jVDrcHKceg6GdJl7--ETsNeFQ1VBFG39Vy_aPd3NuKSBXln5XdH43hbescROWi4NKTPok0KEkxDXsisrsdU7kOJ-KaaW3Yx' }
       )
@@ -73,8 +80,9 @@ class ApplicationController < ActionController::Base
     @address_two = @locationTwo['location']['display_address'][0]
     @loc_one_price = @locationOne['price']
     @loc_two_price = @locationTwo['price']
-
-    p @data.first[1].count
+    @loc_one_distance = @locationOne['distance']
+    @loc_two_distance = @locationTwo['distance']
+    @data.first[1].count
 
     @datae = Curl::Easy.perform("https://www.eventbriteapi.com/v3/events/search/?q=#{query}&sort_by=best&location.address=#{destination}&price=#{event_cost}&token=FGTPMLNV7K6MQVZZCC6S")
 
