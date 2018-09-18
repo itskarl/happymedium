88e54ea63ab3fdaf827345f21b652a154a7a57af
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-17 23:59:13 -0400

    working buy button

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-
   get 'sessions/new'
   resources :itineraries
   resources :events
+  resources :charges
 
   resources :users
   root 'pages#home'
 
   get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-delete '/logout' => 'sessions#destroy'
-get '/logout' => 'sessions#destroy'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
